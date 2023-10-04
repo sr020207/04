a6a705187ffb7253f687e055ef01bf401855940f
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-10-04 23:21:26 +0900

    004

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[]) {
    
-   int x; 
+   int y;
+   int cond1, cond2;
    
-   printf("input the second : ");
-   scanf("%d", &x);
-    
-   printf("the time is %d : %d", x / 60, x % 60);
+   printf("input the year : "); 
+   scanf("%d", &y);
+   
+   cond1 = y%4 == 0 && y%100 != 0;
+   cond2 = y%4 != 0 || y%100 == 0;
+   
+   printf("is the year %d the leap year? : %d", y, cond1); 
    
    return 0;
 }
